3f970ea4deab937275237e8570af5108f884157a
andrys daniel <andrysds@gmail.com>
2019-07-02 15:01:43 +0700

    v1.0.0 handle all http request methods (#2)
    * v1.0.0 handle all http request methods
    
    * fix example url on readme

--- app.rb
+++ app.rb
@@ -7,18 +7,52 @@ before do
 end
 
 get '/*' do
-  if response_exist? request.path_info
-    reply_with request.path_info
+  reply request
+end
+
+post '/*' do
+  reply request
+end
+
+put '/*' do
+  reply request
+end
+
+patch '/*' do
+  reply request
+end
+
+delete '/*' do
+  reply request
+end
+
+options '/*' do
+  reply request
+end
+
+link '/*' do
+  reply request
+end
+
+unlink '/*' do
+  reply request
+end
+
+def reply(req)
+  fpath = req.request_method + req.path_info
+  fpath += 'index' if req.path_info == '/'
+  if response_exist? fpath
+    reply_with fpath
   else
     status 404
-    reply_with '/not_found'
+    reply_with 'not_found'
   end
 end
 
 def response_exist?(fpath)
-  File.exist? "responses#{fpath}.json"
+  File.exist? "responses/#{fpath}.json"
 end
 
 def reply_with(fpath)
-  File.read "responses#{fpath}.json"
+  File.read "responses/#{fpath}.json"
 end

438b01a1e956d97a43a003b00dbe6d314f4cd731
Andrys Daniel Silalahi <andrysds@gmail.com>
2019-07-03 15:37:08 +0700

    fix search adjacent vertex response

--- app.rb
+++ app.rb
@@ -3,16 +3,16 @@
 require 'sinatra'
 
 USER_ACTIVITIES = [
-  { type: 'view_product', pid: 108 },
-  { type: 'add_to_cart', pid: 108 },
-  { type: 'add_to_fav', pid: 108 },
-  { type: 'buy', pid: 108 },
-  { type: 'view_product', pid: 109 },
-  { type: 'add_to_cart', pid: 109 },
-  { type: 'add_to_fav', pid: 109 },
-  { type: 'view_product', pid: 110 },
-  { type: 'add_to_cart', pid: 110 },
-  { type: 'add_to_fav', pid: 110 }
+  { type: 'view_product', pid: 201 },
+  { type: 'view_product', pid: 202 },
+  { type: 'view_product', pid: 203 },
+  { type: 'add_to_cart', pid: 204 },
+  { type: 'add_to_cart', pid: 205 },
+  { type: 'add_to_cart', pid: 206 },
+  { type: 'buy', pid: 207 },
+  { type: 'add_to_fav', pid: 208 },
+  { type: 'add_to_fav', pid: 209 },
+  { type: 'add_to_fav', pid: 210 }
 ].freeze
 
 set :port, (ENV['PORT'] || 4567)
@@ -29,9 +29,9 @@ end
 
 post '/_internal/griffin/vertex/search-adjacent-vertex' do
   build_activities(@edge_filter_values)
-  vertex_edge_data = @activities.map { |x| { product_id: x[:pid] } }
+  vertex_data = @activities.map { |x| { product_id: x[:pid] } }
   {
-    data: { vertex_edge_data: vertex_edge_data, eod: @eod },
+    data: { vertex_data: vertex_data, eod: @eod },
     meta: { http_status: '200' }
   }.to_json
 end

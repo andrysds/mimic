9cbec1f982173d6238e3993fb3a5d2725ef64b4c
Andrys Daniel Silalahi <andrysds@gmail.com>
2019-07-16 17:27:44 +0700

    product category search ability

--- app.rb
+++ app.rb
@@ -2,17 +2,22 @@
 
 require 'sinatra'
 
+PRODUCT_CATEGORIES = {
+  '201' => '8',
+  '202' => '125'
+}.freeze
+
 USER_ACTIVITIES = [
-  { type: 'view_product', pid: '201', created_at: Time.new(2019, 07, 10) },
-  { type: 'view_product', pid: '202', created_at: Time.new(2019, 07, 10) },
-  { type: 'view_product', pid: '203', created_at: Time.new(2019, 07, 10) },
-  { type: 'add_to_cart', pid: '204', created_at: Time.new(2019, 07, 10) },
-  { type: 'add_to_cart', pid: '205', created_at: Time.new(2019, 07, 10) },
-  { type: 'add_to_cart', pid: '206', created_at: Time.new(2019, 07, 10) },
-  { type: 'buy', pid: '207', created_at: Time.new(2019, 07, 10) },
-  { type: 'add_to_fav', pid: '208', created_at: Time.new(2019, 07, 10) },
-  { type: 'add_to_fav', pid: '209', created_at: Time.new(2019, 07, 10) },
-  { type: 'add_to_fav', pid: '210', created_at: Time.new(2019, 07, 10) }
+  { type: 'view_product', pid: '201', created_at: Time.new(2019, 7, 10) },
+  { type: 'view_product', pid: '202', created_at: Time.new(2019, 7, 10) },
+  { type: 'view_product', pid: '203', created_at: Time.new(2019, 7, 10) },
+  { type: 'add_to_cart', pid: '204', created_at: Time.new(2019, 7, 10) },
+  { type: 'add_to_cart', pid: '205', created_at: Time.new(2019, 7, 10) },
+  { type: 'add_to_cart', pid: '206', created_at: Time.new(2019, 7, 10) },
+  { type: 'buy', pid: '207', created_at: Time.new(2019, 7, 10) },
+  { type: 'add_to_fav', pid: '208', created_at: Time.new(2019, 7, 10) },
+  { type: 'add_to_fav', pid: '209', created_at: Time.new(2019, 7, 10) },
+  { type: 'add_to_fav', pid: '210', created_at: Time.new(2019, 7, 10) }
 ].freeze
 
 set :port, (ENV['PORT'] || 4567)
@@ -22,14 +27,22 @@ before do
 
   request.body.rewind
   @request_payload = JSON.parse request.body.read
+  @vertex_start_filter_keys = @request_payload['vertex_start_filter_keys']
+  @vertex_start_filter_values = @request_payload['vertex_start_filter_values']
   @edge_filter_values = @request_payload['edge_filter_values']
   @offset = @request_payload['offset'].to_i
   @limit = (@request_payload['limit'] || 1).to_i
 end
 
 post '/_internal/griffin/vertex/search-adjacent-vertex' do
-  build_activities(@edge_filter_values)
-  vertex_data = @activities.map { |x| { product_id: x[:pid] } }
+  vertex_data = []
+  if @vertex_start_filter_keys&.first == 'product_id'
+    cid = PRODUCT_CATEGORIES[@vertex_start_filter_values&.first] || "0"
+    vertex_data = [{ "category_id": cid }]
+  else
+    build_activities(@edge_filter_values)
+    vertex_data = @activities.map { |x| { product_id: x[:pid] } }
+  end
   {
     data: { vertex_data: vertex_data, eod: @eod },
     meta: { http_status: '200' }
